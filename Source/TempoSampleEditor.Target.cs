32339a99b6a13598f91dd49569a6c3a0492ce767
Peter Melick <pete@temposimulation.com>
2026-02-15 20:58:33 -0500

    Use Latest Build Settings Version

--- Source/TempoSampleEditor.Target.cs
+++ Source/TempoSampleEditor.Target.cs
@@ -8,7 +8,7 @@ public class TempoSampleEditorTarget : TargetRules
 	public TempoSampleEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("TempoSample");
 
